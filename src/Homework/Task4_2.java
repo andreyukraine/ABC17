200242fa36bc501c51d11f4152b794cec9850fbc
IT <security_ua@mail.ru>
2016-06-29 11:51:14 +0300

    Andrey

--- src/Homework/Task4_2.java
+++ src/Homework/Task4_2.java
@@ -15,8 +15,21 @@ public class Task4_2 {
         int number2 = scanner.nextInt();
         int number3 = scanner.nextInt();
 
+        if (number1 >= number2 & number1 >= number3){
+            System.out.println("наибольшее number1 " + number1);
+        }else if (number2 >= number1 & number2 >= number3){
+            System.out.println("наибольшее number2 " + number2);
+        }else if (number3 >= number1 & number3 >= number2){
+            System.out.println("наибольшее number3 " + number3);
+        }
 
-
+        if (number1 <= number2 & number1 <= number3){
+            System.out.println("наименьшее number1 " + number1);
+        }else if (number2 <= number1 & number2 <= number3){
+            System.out.println("наименьшее number2 " + number2);
+        }else if (number3 <= number1 & number3 <= number2){
+            System.out.println("наименьшее number3 " + number3);
+        }
 
     }
 }
--- src/Homework/Task4_7.java
+++ src/Homework/Task4_7.java
@@ -1,5 +1,7 @@
 package Homework;
 
+import java.util.Scanner;
+
 /**
  * Created by IT on 27.06.2016.
  */
@@ -7,4 +9,12 @@ public class Task4_7 {
     //4.7. Вводим два числа, если одно из них делиться на другое без остатка, то выводим тру и показываем результат деления
     // (целую часть от деления  и остачу) в другом случае выводим false и показываем результат деления
     // (целую часть от деления  и остачу)
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+
+    }
 }
