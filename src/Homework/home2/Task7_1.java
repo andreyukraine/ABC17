416e9af73ddef4d2f86ace9865d6a828de3df828
IT <security_ua@mail.ru>
2016-07-05 07:22:33 +0300

    Andrey

--- src/Homework/home2/Task7_1.java
+++ src/Homework/home2/Task7_1.java
@@ -1,9 +1,29 @@
 package Homework.home2;
 
+import ua.artcode.week1.ArrayUtils;
+
 /**
  * Created by IT on 05.07.2016.
  */
 public class Task7_1 {
 //    7.1 Инвертировать массив (без использования допольнительного массива)
+public static void main(String[] args) {
+    int[] mass = ArrayUtils.createAndFillRandomArray(5, 0, 10);
+
+    ArrayUtils.printArray(mass);
+
+    //Делим массив пополам
+    for (int i = 0; i < mass.length/2; i++) {
+            //Временная переменая для записи предыдущего числа с индексом
+            int temp = mass[i];
+            //Записываем в левую ячейку массива самую правую
+            mass[i] = mass[mass.length - i - 1];
+            //Записываем в правую ячейку массива временную переменную
+            mass[mass.length - i -1] = temp;
+
+    }
+
+    ArrayUtils.printArray(mass);
+}
 
 }
--- src/Homework/home2/Task8.java
+++ src/Homework/home2/Task8.java
@@ -1,5 +1,7 @@
 package Homework.home2;
 
+import java.util.Scanner;
+
 /**
  * Created by IT on 05.07.2016.
  */
@@ -10,4 +12,44 @@ public class Task8 {
 //    Число попыток ограничено 5.
 //    Если пользователь ввел неверное число, программа дает подсказу (больше/меньше).
 //    Придумать разные варианты ответов, при отгадывании с первой попытки, нескольких попыток, в случае проигрыша.
+
+    public static void main(String[] args) {
+        int number = (int)(25 + Math.random() * (125 - 25));
+        System.out.println(number);
+
+        Scanner scanner = new Scanner(System.in);
+        int input_number = 0;
+        int ansver = number;
+        int i = 1;
+        System.out.println("Угадайте число, у Вас п'ять попыток!!!!!");
+        do {
+
+
+            if (i <= 5) {
+
+                System.out.println("Введите число от 25 - 125");
+                input_number = scanner.nextInt();
+
+                if (input_number == ansver) {
+                    System.out.println("Вы угадали");
+                    break;
+                    } else {
+                        if (input_number > ansver) {
+                            System.out.println("Число больше");
+                        } else {
+                            System.out.println("Число меньше");
+                        }
+                        System.out.println("Попробуйте еще раз");
+                        ++i;
+                    }
+
+            }else{
+                System.out.println("У Вас больше нет попыток для ввода числа");
+                break;
+            }
+
+        }while (input_number !=0);
+
+    }
+
 }
