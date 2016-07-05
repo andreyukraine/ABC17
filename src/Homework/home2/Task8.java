43718cc174b9bf0899cbea405f453a519622b9a1
IT <security_ua@mail.ru>
2016-07-05 15:47:17 +0300

    Andrey

--- src/Homework/home2/Task8.java
+++ src/Homework/home2/Task8.java
@@ -14,32 +14,46 @@ public class Task8 {
 //    Придумать разные варианты ответов, при отгадывании с первой попытки, нескольких попыток, в случае проигрыша.
 
     public static void main(String[] args) {
+
+        //Геннерируем массив
         int number = (int)(25 + Math.random() * (125 - 25));
         System.out.println(number);
 
         Scanner scanner = new Scanner(System.in);
+
+        //Введеное число пользователем
         int input_number = 0;
+
+        //Загаданное сгенерированое число
         int ansver = number;
+
+        //Количество попыток
+        int try_input = 5;
+
+        //Шаг количества попыток
         int i = 1;
+
         System.out.println("Угадайте число, у Вас п'ять попыток!!!!!");
         do {
 
-
-            if (i <= 5) {
+            if (i <= try_input) {
 
                 System.out.println("Введите число от 25 - 125");
                 input_number = scanner.nextInt();
 
                 if (input_number == ansver) {
-                    System.out.println("Вы угадали");
+                    System.out.println("Ура!!!!! Вы угадали");
                     break;
                     } else {
+                    if (i <= 4) {
                         if (input_number > ansver) {
-                            System.out.println("Число больше");
+                            System.out.println("Число больше чем загаданое");
                         } else {
-                            System.out.println("Число меньше");
+                            System.out.println("Число меньше чем загаданое");
                         }
-                        System.out.println("Попробуйте еще раз");
+                        System.out.println("Попробуйте еще раз! У Вас еще " + (try_input-i) + " попыток" );
+                        System.out.println();
+                    }
                         ++i;
                     }
 
